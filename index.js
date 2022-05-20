d7bb4b1ad3f28f907340b1ac9b16a4f7cba6726f
Tanmay Kumar <itstanmaykumar@gmail.com>
2022-05-21 03:34:54 +0600

    get burgers added

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@gatsbee.wbrar.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Gatsbee DB Connected!");
+
+        const burgersCollection = database.collection('burgers');
+
+
+        // getting all burgers
+        app.get('/burgers', async (req, res) => {
+            const cursor = burgersCollection.find({});
+            const burgers = await cursor.toArray();
+            res.send(burgers);
+        });
+
+
+    }
+    finally {
+        // finally lorem :p
+    }
+}
+
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello Bohemian!')
+})
+
+app.listen(port, () => {
+    console.log(`listening at ${port}`)
+})
